bfd8fc7ff494cbdd1f4436d559881e6bdafbd6fa
Austin McCall <85043551+DeathGamer111@users.noreply.github.com>
2023-01-30 01:20:44 -0600

    Update provider.ts

--- utils/provider.ts
+++ utils/provider.ts
@@ -3,16 +3,11 @@ import sample from "lodash/sample";
 import { NETWORK } from ".";
 
 const MAINNET_RPC = [
-  "https://bsc-dataseed.binance.org/",
-  "https://bsc-dataseed1.defibit.io/",
-  "https://bsc-dataseed1.ninicoin.io/",
+  "http://rpc.gaussgang.com/",
 ];
 
 const TESTNET_RPC = [
-  "https://data-seed-prebsc-1-s1.binance.org:8545/",
-  "https://data-seed-prebsc-1-s2.binance.org:8545/",
-  "https://data-seed-prebsc-2-s1.binance.org:8545/",
-  "https://data-seed-prebsc-2-s2.binance.org:8545/",
+  "https://rpc.giltestnet.com",
 ];
 
 const provider = new providers.JsonRpcProvider(NETWORK === "testnet" ? sample(TESTNET_RPC) : sample(MAINNET_RPC));
